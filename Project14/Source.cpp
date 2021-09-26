dd05840fde2d3233ee3965b43516af6e64985b27
PAWAT YAMWONG <64010646@KMITL.AC.TH>
2021-09-26 23:45:50 +0700

    Add project files.

---
+++ Project14/Source.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void Array_sort(int* array, int n)
+{
+    int i = 0, j = 0, temp = 0;
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
+float Find_median(int array[], int n)
+{
+    float median = 0;
+    if (n % 2 == 0)
+        median = (array[(n - 1) / 2] + array[n / 2]) / 2.0;
+    else
+        median = array[n / 2];
+
+    return median;
+}
+
+int main()
+{
+    int array_1[10] = { 0 };
+    int i = 0, n = 10;
+    float median = 0;
+    printf("Enter your number \n");
+    for (i = 0; i < n; i++)
+    {
+        printf("number[%d] : ", i+1);
+        scanf_s("%d", &array_1[i]);
+    }
+    Array_sort(array_1, n);
+    median = Find_median(array_1, n);
+
+    printf("\n\nThe median is : %f\n", median);
+    return 0;
+}
